a9dc836d64afd94175bf1497fd5615b00fb524b5
ccabral99 <cabralcamila1999@gmail.com>
2019-06-19 13:29:54 -0300

    holaaa

--- funciones.js
+++ funciones.js
@@ -41,7 +41,7 @@ function func(){
     document.getElementById("mostrarCalculos").innerHTML += "<p>Resolver primer grado:</p><p>Paso uno: " +Number(document.ejercicios.X.value)+"x="+Number(document.ejercicios.resultado.value)+"-"+Number(document.ejercicios.Nro.value)+"</p>";
     divMostrar.innerHTML+="<p>Paso dos: x="+ (Number(document.ejercicios.resultado.value)-Number(document.ejercicios.Nro.value))+" / "+ Number(document.ejercicios.X.value)+"</p>";
     divMostrar.innerHTML+="<p> Resolver segundo Grado:</p><p>Aplicamos Bhaskara:</p>"+" <P>- "+ Number(document.ejercicios.Xdos.value)+"+<p> determinante: raiz cuadrada de "+Number(document.ejercicios.Xdos.value)+" elevado al cuadrado - 4* "+Number(document.ejercicios.Xcuadrado.value)+"*"+Number(document.ejercicios.Nrodos.value)+" dividido 2* "+Number(document.ejercicios.Xcuadrado.value)+"</p>"
-    +"<p> IMPORTANTE: RECORDAR QUE DE LA RAIZ SE OBTIENEN UN VALOR POSITIVO Y OTRO NEGATIVO Y ALLI OBTENEMOS LAS DOS RAICES</p>";}
+        +"<p> IMPORTANTE: RECORDAR QUE DE LA RAIZ SE OBTIENEN UN VALOR POSITIVO Y OTRO NEGATIVO Y ALLI OBTENEMOS LAS DOS RAICES</p>";}
 
 
 function dibujarCuadriculado (){{
@@ -49,7 +49,6 @@ function dibujarCuadriculado (){{
     var ctx = canvas.getContext("2d");
 
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    ctx.fillStyle= "#0000";
     ctx.beginPath();
     for (var i=10;i<canvas.width;i+=10) {
         ctx.moveTo(i, 0);
@@ -60,33 +59,35 @@ function dibujarCuadriculado (){{
         ctx.moveTo(0, i);
         ctx.lineTo(canvas.width, i);
     }
+    ctx.strokeStyle="#fc29ef";
     ctx.stroke();
 
 
 }
-ctx.fillStyle="#ff6072"
-ctx.moveTo(0,canvas.height/2);
-ctx.lineTo(canvas.width,canvas.height/2);
-ctx.moveTo(canas.width/2,0);
-ctx.lineTo(canvas.width/2,canvas.height);
-ctx.stroke();
+    ctx.strokeStyle="#19090b"
+    ctx.moveTo(0,canvas.height/2);
+    ctx.lineTo(canvas.width,canvas.height/2);
+    ctx.moveTo(canvas.width/2,0);
+    ctx.lineTo(canvas.width/2,canvas.height);
+    ctx.stroke();
 
     ctx.beginPath();
 }
-    function res()
-    {
-        a=document.datos.a.value;b=document.datos.b.value;c=document.datos.c.value;
-        d=document.datos.d.value;e=document.datos.e.value;f=document.datos.f.value;
-        g=a*e-b*d; if(g==0)
-    {if (c*e-b*f==0) {x="Indeterminado";y="Indeterminado"}
-    else{x="Inconpatible";y="Incompatible"}
-    }
-    else{x=(c*e-b*f)/g;y=(a*f-c*d)/g}; document.datos.x.value=x;document.datos.y.value=y;
-    }
-/*function dibujarSistemas() {
+function res()
+{
+    a=document.datos.a.value;b=document.datos.b.value;c=document.datos.c.value;
+    d=document.datos.d.value;e=document.datos.e.value;f=document.datos.f.value;
+    g=a*e-b*d; if(g==0)
+{if (c*e-b*f==0) {x="Indeterminado";y="Indeterminado"}
+else{x="Inconpatible";y="Incompatible"}
+}
+else{x=(c*e-b*f)/g;y=(a*f-c*d)/g}; document.datos.x.value=x;document.datos.y.value=y;
+}
 
-            a=document.datos.a.value;b=document.datos.b.value;c=document.datos.c.value;
-            d=document.datos.d.value;e=document.datos.e.value;f=document.datos.f.value;
-            g=a*e-b*d;
-            x=(c*e-b*f)/g;y=(a*f-c*d)/g;
-            document.datos.x.value=x;document.datos.y.value=y;*/
+function dibujarInterseccion(a, d) {
+    ctx.fillStyle = "#af3679";
+    ctx.beginPath();
+    ctx.arc(canvas.width/2,canvas.width/2,a,d);
+    ctx.fill();
+    ctx.closePath();
+}
