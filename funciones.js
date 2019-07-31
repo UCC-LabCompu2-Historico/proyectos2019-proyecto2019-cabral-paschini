8134714eced99b6d775bcb0206699669d89cf538
cata99 <48757813+cata99@users.noreply.github.com>
2019-07-31 01:13:27 -0300

    actualice ghpages

--- funciones.js
+++ funciones.js
@@ -105,6 +105,15 @@ function func() {
         + "<p><u>IMPORTANTE:</u> RECORDAR QUE DE LA RAIZ SE OBTIENEN UN VALOR POSITIVO Y OTRO NEGATIVO </p>";
 }
 
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+/**
+Se muestra un recuadro cuadriculado con los ejes de Coordenadas
+@method  function dibujarCuadriculado
+@return hoja cuadriculada con los ejes de coordenadas
+**/
 function dibujarCuadriculado() {
     {
         var canvas = document.getElementById("myCanvas");
@@ -113,11 +122,13 @@ function dibujarCuadriculado() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         ctx.beginPath();
         for (var i = 10; i < canvas.width; i += 10) {
+
             ctx.moveTo(i, 0);
             ctx.lineTo(i, canvas.height);
         }
 
         for (var i = 10; i < canvas.width; i += 10) {
+
             ctx.moveTo(0, i);
             ctx.lineTo(canvas.width, i);
         }
@@ -127,56 +138,203 @@ function dibujarCuadriculado() {
 
     }
 
+    ejeX(ctx,canvas.height,canvas.width);
+    ejeY(ctx,canvas.height,canvas.width);
 
+}
+
+/**
+Aparece los ejes de manera animada
+@method  function ejeX
+ @param ctx
+ @param canvas.height
+ @param canvas.width
+ @return el eje dibujado
+**/
+
+async function ejeX(ctx,alto,ancho){
     ctx.lineWidth = 2;
     ctx.beginPath();
     ctx.strokeStyle = "#000000";
-
-    ctx.moveTo(0, canvas.height / 2);
-    ctx.lineTo(canvas.width, canvas.height / 2);
-
-    ctx.moveTo(canvas.width / 2, 0);
-    ctx.lineTo(canvas.width / 2, canvas.height);
-
-    ctx.stroke();
-    ctx.closePath();
-
-}
-
-function res() {
-    a = document.datos.a.value;
-    b = document.datos.b.value;
-    c = document.datos.c.value;
-    d = document.datos.d.value;
-    e = document.datos.e.value;
-    f = document.datos.f.value;
-    g = a * e - b * d;
-    if (g == 0) {
-        if (c * e - b * f == 0) {
-            x = "Indeterminado";
-            y = "Indeterminado"
-        } else {
-            x = "Inconpatible";
-            y = "Incompatible"
-        }
-    } else {
-        x = (c * e - b * f) / g;
-        y = (a * f - c * d) / g
+    for(let i = 0; i<ancho ; i++){
+        await sleep(5);
+        ctx.moveTo(0,alto/2);
+        ctx.lineTo(i,alto/2);
+        ctx.stroke();
     }
-    ;document.datos.x.value = x;
-    document.datos.y.value = y;
+    ctx.closePath();
 }
 
-function DibujeInterseccion(x, y, x, y) {
+
+/**
+ Aparece los ejes de manera animada
+ @method  function ejeY
+ @param ctx
+ @param canvas.height
+ @param canvas.width
+ @return el eje dibujado
+ **/
+
+async function ejeY(ctx,alto,ancho){
+    ctx.lineWidth = 2;
+    ctx.beginPath();
+    ctx.strokeStyle = "#000000";
+    for(let j = 0; j < alto ; j++){
+        await sleep(5);
+        ctx.moveTo(ancho/2,0);
+        ctx.lineTo(ancho/2, j);
+        ctx.stroke();
+    }
+    ctx.closePath();
+}
+/**
+Se muestra las rectas del Sistema de Coordenadas
+@method function marcarRecta
+@return dibuja las restas del sistema de ecuaciones
+**/
+async function marcarRecta(){
+	    var canvas = document.getElementById("myCanvas");
+	    ctx = canvas.getContext("2d");
+	    ctx.lineWidht= 2;
+	    ctx.strokeStyle= "#001f33";
+	    var x, y;
+
+	    var vara=document.getElementById("a").value;
+	    var varb=document.getElementById("b").value;
+	    var varc=document.getElementById("c").value;
+	    console.log("Valores de entrada: "+vara+" ,"+varb+" ,"+varc);
+
+	    ctx.beginPath();
+	    //Valor de inicio de la recta
+	    x=50;
+
+
+
+	    y=(varc-vara*x)/varb;
+	    console.log("valor X: " + 50);
+	    console.log("valor y: " + y);
+
+
+	    dibX = transX(x);
+	    dibY = transY(y);
+	    console.log("X trans: "+ dibX + "Y trans: "+ dibY);
+	    ctx.moveTo(dibX, dibY);
+
+	    //Valor final de la recta
+
+	    x=-50;
+	    y=(varc-vara*x)/varb;
+	    console.log("valor X: " + -50);
+	    console.log("valor y: " + y);
+	    dibX = transX(x);
+	    dibY = transY(y);
+	    console.log("X trans: "+ dibX + "Y trans: "+ dibY);
+	    ctx.lineTo(dibX, dibY);
+	    ctx.stroke();
+	    ctx.closePath();
+
+
+
+
+	    var vara=document.getElementById("d").value;
+	    var varb=document.getElementById("e").value;
+	    var varc=document.getElementById("f").value;
+	    console.log("Valores de entrada: "+vara+" ,"+varb+" ,"+varc);
+
+	    ctx.beginPath();
+	    //Valor de inicio de la recta
+	    x=50;
+	    y=(varc-vara*x)/varb;
+	    console.log("valor X: " + 50);
+	    console.log("valor y: " + y);
+
+	    dibX = transX(x);
+	    dibY = transY(y);
+	    console.log("X trans: "+ dibX + "Y trans: "+ dibY);
+	    ctx.moveTo(dibX, dibY);
+
+	    //Valor final de la recta
+	    x=-50;
+	    y=(varc-vara*x)/varb;
+	    console.log("valor X: " + -50);
+	    console.log("valor y: " + y);
+	    dibX = transX(x);
+	    dibY = transY(y);
+	    console.log("X trans: "+ dibX + "Y trans: "+ dibY);
+	    ctx.lineTo(dibX, dibY);
+	    ctx.stroke();
+	    ctx.closePath();
+	}
+/**
+Se traslada la recta en el eje y
+@method function transY
+@return se mueve la recta en el eje y
+**/
+	function transY(coorY) {
+	    var trY = -(coorY-25)*10;
+
+	    return trY;
+	}
+/**
+Se traslada la recta en el eje x
+@method function de transX
+@return traslada de la recta en el eje x
+**/
+	function transX(coorX) {
+	    var trX = (coorX+50)*10;
+
+	    return trX;
+	}
+
+
+  /**
+   * Calculo del sistemas de ecuaciones
+   * @method res;
+   * @param Parámetro a;
+   * @param Parámetro b;
+   * @param Parámetro c;
+   * @param Parámetro d;
+   *@param Parámetro e;
+   *@param Parámetro f;
+   * @return x,y;
+   */
+
+  function res()
+  	{
+  	   a=document.datos.a.value;
+      b=document.datos.b.value;
+      c=document.datos.c.value;
+      d=document.datos.d.value;
+      e=document.datos.e.value;
+      f=document.datos.f.value;
+      g=a*e-b*d;
+      if(g==0)
+  {if (c*e-b*f==0)
+    {x="Indeterminado";y="Indeterminado"}
+  else
+  {x="Inconpatible";y="Incompatible"}
+  }
+  else
+  {x=(c*e-b*f)/g;
+    y=(a*f-c*d)/g};
+    document.datos.x.value=x;
+    document.datos.y.value=y;
+  }
+  /**
+  * Con los valores anteriores realiza el grafico
+ * @method DibujeInterseccion;
+ * @return grafica;
+  **/
+  function DibujeInterseccion(x, y, x, y) {
 
     function res() {
         ctx.beginPath();
 
         ctx.moveTo(x, y);
 
-        ctx.lineTo(x1, y1);
+        ctx.lineTo(x, y);
 
         ctx.stroke();
-    }
 
+    }
 }
